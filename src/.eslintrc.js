94c559c1ad4b71af0713c136c85178cdb3e5533d
anindyoo <anindyo0216@gmail.com>
2024-05-04 19:20:15 +0700

    feat: setup eslint configs.

---
+++ src/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['react-app', 'airbnb', 'plugin:jsx-a11y/recommended'],
+  plugins: ['jsx-a11y'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+      },
+    ],
+    semi: 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+
+  },
+}
