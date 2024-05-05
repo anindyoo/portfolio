7054253617cedab5f992e99b9793f406678e58be
anindyoo <anindyo0216@gmail.com>
2024-05-05 15:30:45 +0700

    feat: create app react router & add tailwind stylings.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
-const App = () => {
-  return (
-    <div className="App">
+/* eslint-disable import/no-extraneous-dependencies */
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home/Home';
 
+/* eslint-disable arrow-body-style */
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+    },
+  ]);
+  return (
+    <div className="
+    APP
+    relative
+    box-border"
+    >
+      <div className="
+      SCREEN-FRAME-CONTAINER
+      fixed z-[-10]
+      w-full h-screen
+      p-3"
+      >
+        <div className="
+        SCREEN-FRAME
+        h-full
+        border border-black box"
+        />
+      </div>
+      <main className="
+      MAIN-CONTAINER
+      px-7 py-5"
+      >
+        <RouterProvider router={router} />
+      </main>
     </div>
   );
-}
+};
 
 export default App;
