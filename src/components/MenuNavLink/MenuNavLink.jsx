1a6ed89637928cbff1f71b21f3701e6daffa7784
anindyoo <anindyo0216@gmail.com>
2024-05-06 22:14:10 +0700

    feat: udpate classnames.

--- src/components/MenuNavLink/MenuNavLink.jsx
+++ src/components/MenuNavLink/MenuNavLink.jsx
@@ -2,10 +2,10 @@ import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const MenuNavLink = (props) => {
-  const { name, link } = props;
+  const { className, name, link } = props;
   const [isHovered, setIsHovered] = useState(false)
 
-  const hoveredStyle = 'italic font-bold text-white';
+  const hoveredStyle = 'italic font-bold text-white text-stroke';
   const loopSize = 5;
 
   const onMouseEnterHandler = () => setIsHovered(true);
@@ -17,40 +17,46 @@ const MenuNavLink = (props) => {
       to={link}
       onMouseEnter={onMouseEnterHandler}
       onMouseLeave={onMouseLeaveHandler}
-      className="
+      className={`
+      ${className} 
       NAVLINK-CONTAINER
-      flex flex-col items-center
-      relative"
+      flex flex-col items-end w-fit
+`}
     >
       <span className={`
-      NAVLINK-MAIN-TEXT
-      transition-all duration-100
-      ${isHovered && hoveredStyle}`}
+      NAVLINK-TEXT-CONTAINER
+      relative
+      transition-all duration-100`}
       >
-        {name}
-      </span>
-      {Array.from(Array(loopSize), (_e, i) => (
-        <span
-          key={i}
-          className={`
-          NAVLINK-SHADOW-TEXT
-          absolute
-          flex flex-row items-center         
-          ease-in-out
-          top-0
-          ${isHovered ? 'visible font-bold italic' : 'font-normal opacity-0 invisible'}`}
-          style={{
-            transitionDuration: isHovered
-              ? `${(loopSize - i + 1) * 0.04}s`
-              : `${(loopSize - i + 1) * 0.08}s`,
-            zIndex: `${i + 1 * -10}`,
-            opacity: `${(loopSize - i) / (loopSize + 1)}`,
-            transform: isHovered && `translate(0, ${(i + 1) * 12}px)`,
-          }}
+        <span className={`
+        NAVLINK-MAIN-TEXT"        
+        ${isHovered && hoveredStyle}`}
         >
           {name}
         </span>
-      ))}
+        {Array.from(Array(loopSize), (_e, i) => (
+          <span
+            key={i}
+            className={`
+            NAVLINK-SHADOW-TEXT
+            absolute
+            flex flex-row items-center         
+            ease-in-out
+            top-0
+            ${isHovered ? 'visible font-bold italic' : 'font-normal opacity-0 invisible'}`}
+            style={{
+              transitionDuration: isHovered
+                ? `${(loopSize - i + 1) * 0.1}s`
+                : `${(loopSize - i + 1) * 0.14}s`,
+              zIndex: `${i + 1 * -10}`,
+              opacity: `${(loopSize - i) / (loopSize + 1)}`,
+              transform: isHovered && `translate(0, ${(i + 1) * 12}px)`,
+            }}
+          >
+            {name}
+          </span>
+        ))}
+      </span>
     </NavLink>
   );
 };
