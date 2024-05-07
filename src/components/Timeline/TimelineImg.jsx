9aa94a79637b092a652051c6502ab5ff4b334b2b
anindyoo <anindyo0216@gmail.com>
2024-05-07 22:54:49 +0700

    feat: create timeline image componenet.

---
+++ src/components/Timeline/TimelineImg.jsx
@@ -0,0 +1,16 @@
+const TimelineImg = () => {
+  console.log()
+
+  return (
+    <div className={`
+      TIMELINE-IMAGE
+      border
+      w-full h-screen max-h-[33.125rem]
+      flex`}
+    >
+      THE IMAGE
+    </div>
+  );
+};
+
+export default TimelineImg;
