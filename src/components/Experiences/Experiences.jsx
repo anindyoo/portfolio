2dba663197b0515686efda9e944b2c9639744762
anindyoo <anindyo0216@gmail.com>
2024-05-08 21:09:53 +0700

    feat: create component for experiences section.

---
+++ src/components/Experiences/Experiences.jsx
@@ -0,0 +1,14 @@
+const Experiences = () => {
+  console.log();
+
+  return (
+    <section className="
+      EXPERIENCES-SECTION
+      h-screen"
+    >
+      EXPERIENCES SECTION
+    </section>
+  );
+};
+
+export default Experiences;
