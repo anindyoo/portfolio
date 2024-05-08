c387ff10f67a7e0b9bd596d87352b0abe692afb3
anindyoo <anindyo0216@gmail.com>
2024-05-08 14:42:47 +0700

    feat: set img size and add dummy image.

--- src/components/Timeline/TimelineImg.jsx
+++ src/components/Timeline/TimelineImg.jsx
@@ -3,12 +3,18 @@ const TimelineImg = () => {
 
   return (
     <div className={`
-      TIMELINE-IMAGE
-      border
-      w-full h-screen max-h-[33.125rem]
-      flex`}
+      TIMELINE-IMAGE-CONTAINER
+      w-full h-screen max-h-[calc(100vh*(66.25/100))]
+      flex items-center justify-center`}
     >
-      THE IMAGE
+      <img
+        src="https://i.scdn.co/image/ab67616d0000b2731572698fff8a1db257a53599"
+        alt="alt"
+        className="
+        TIMELINE-IMAGE
+        object-cover
+        w-[calc(100vw*(40.28/100))] h-[calc(100vh*(56.25/100))]"
+      />
     </div>
   );
 };
