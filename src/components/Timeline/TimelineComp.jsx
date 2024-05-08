48fcb36e3d0206269068b2032827afe16269ca27
anindyoo <anindyo0216@gmail.com>
2024-05-08 14:41:57 +0700

    feat: add isLast prop.

--- src/components/Timeline/TimelineComp.jsx
+++ src/components/Timeline/TimelineComp.jsx
@@ -5,10 +5,11 @@ const TimelineComp = (props) => {
   const {
     title,
     isOdd,
+    isLast,
   } = props;
 
   const descAttributes = {
-    title, isOdd,
+    title, isOdd, isLast,
   };
 
   return (
@@ -26,24 +27,26 @@ const TimelineComp = (props) => {
         </>
 
       )}
-      <div className={
-      `TIMLINE-BUFFER-CONTAINERE
-      relative 
-      h-screen max-h-60
-      grid grid-cols-subgrid col-span-2`
-      }
-      >
-        <div className={`
-        TIMLINE-BUFFER-BORDER
-        w-full h-[calc(100%+2px)]
-        absolute
-        top-[-1px]
-        border-[-2px] border-t border-black
-        ${isOdd
-          ? 'col-start-2 border-r rounded-tr-[12.5rem]'
-          : 'col-start-1 w-[50%] border-l rounded-tl-[12.5rem]'}`}
-        />
-      </div>
+      {!isLast && (
+        <div className={
+        `TIMLINE-BUFFER-CONTAINERE
+        relative 
+        h-screen max-h-60
+        grid grid-cols-subgrid col-span-2`
+        }
+        >
+          <div className={`
+          TIMLINE-BUFFER-BORDER
+          w-full h-[calc(100%+2px)]
+          absolute
+          top-[-1px]
+          border-[-2px] border-t border-black
+          ${isOdd
+            ? 'col-start-2 border-r rounded-tr-[12.5rem]'
+            : 'col-start-1 w-[50%] border-l rounded-tl-[12.5rem]'}`}
+          />
+        </div>
+      )}
     </>
   );
 };
