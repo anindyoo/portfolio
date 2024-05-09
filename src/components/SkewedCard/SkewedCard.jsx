0e5e5a0dd9e0a328ec7c0df1dff0869be5fc8941
anindyoo <anindyo0216@gmail.com>
2024-05-09 22:30:40 +0700

    feat: add dummy props.

--- src/components/SkewedCard/SkewedCard.jsx
+++ src/components/SkewedCard/SkewedCard.jsx
@@ -1,11 +1,19 @@
-const SkewedCard = () => {
-  console.log();
+const SkewedCard = (props) => {
+  const {
+    desc,
+  } = props;
 
   return (
-    <div className="
-      SKEWED-CARD"
+    <div
+      className="
+      SKEWED-CARD
+      border border-red-700
+      absolute
+      h-[15rem] w-[37.5rem]"
     >
       card
+      {' '}
+      {desc}
     </div>
   );
 };
