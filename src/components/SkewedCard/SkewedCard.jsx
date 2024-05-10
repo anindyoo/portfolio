79ca8ad7b98aac4d22ad3d183556a6557e5e3b5e
anindyoo <anindyo0216@gmail.com>
2024-05-10 13:27:03 +0700

    feat: add skewed card container.

--- src/components/SkewedCard/SkewedCard.jsx
+++ src/components/SkewedCard/SkewedCard.jsx
@@ -1,19 +1,26 @@
 const SkewedCard = (props) => {
   const {
     desc,
+    isOdd,
   } = props;
 
   return (
     <div
       className="
       SKEWED-CARD
-      border border-red-700
       absolute
-      h-[15rem] w-[37.5rem]"
+      box-border"
     >
-      card
-      {' '}
-      {desc}
+      <div className={`
+      SKEWED-CARD-CONTAINER
+      border border-blue-800      
+      h-[15rem] w-[37.5rem]
+      ${isOdd ? 'skew-y-[15deg]' : '-skew-y-[15deg]'}`}
+      >
+        card
+        {' '}
+        {desc}
+      </div>
     </div>
   );
 };
