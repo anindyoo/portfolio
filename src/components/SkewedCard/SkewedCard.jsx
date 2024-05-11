8ed0aaf4364fb22389ff99cbf3bb38134369c63b
anindyoo <anindyo0216@gmail.com>
2024-05-11 16:54:18 +0700

    feat: add content into skewed card container.

--- src/components/SkewedCard/SkewedCard.jsx
+++ src/components/SkewedCard/SkewedCard.jsx
@@ -13,13 +13,27 @@ const SkewedCard = (props) => {
     >
       <div className={`
       SKEWED-CARD-CONTAINER
-      border border-blue-800      
+      border border-blue-800
+      flex flex-col gap-6
       h-[15rem] w-[37.5rem]
+      px-6 py-5
       ${isOdd ? 'skew-y-[15deg]' : '-skew-y-[15deg]'}`}
       >
-        card
-        {' '}
-        {desc}
+        <div className="
+        CARD-TITLE-CONTAINER
+        flex flex-col"
+        >
+          <span className="
+          CARD-TITLE
+          text-5xl"
+          >
+            Front End Engineer
+            {' '}
+            {desc}
+          </span>
+          <span className="CARD-COMPANY text-2xl">Company name</span>
+        </div>
+        <span className="text-base font-light">September 2021 — February 2024</span>
       </div>
     </div>
   );
