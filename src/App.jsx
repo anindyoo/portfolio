36f4305a0fd253ce6318c3d2b7d40ac56a610ed4
anindyoo <anindyo0216@gmail.com>
2024-05-15 20:34:07 +0700

    feat: add footer component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Footer from './components/Footer/Footer';
 
 /* eslint-disable arrow-body-style */
 const App = () => {
@@ -18,9 +19,9 @@ const App = () => {
     >
       <div className="
       SCREEN-FRAME-CONTAINER
-      fixed z-[-10]
+      fixed z-10
       w-full h-screen
-      p-3"
+      p-3 pointer-events-none"
       >
         <div className="
         SCREEN-FRAME
@@ -34,6 +35,7 @@ const App = () => {
       >
         <RouterProvider router={router} />
       </main>
+      <Footer />
     </div>
   );
 };
