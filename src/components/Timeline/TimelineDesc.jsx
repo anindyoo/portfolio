2974e8a1a7bcae735e69d7ab176a8530877f0af2
anindyoo <anindyo0216@gmail.com>
2024-05-16 21:56:11 +0700

    feat: update timline border using masking & pseudo element.

--- src/components/Timeline/TimelineDesc.jsx
+++ src/components/Timeline/TimelineDesc.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const TimelineDesc = (props) => {
   const {
     title,
@@ -5,41 +6,34 @@ const TimelineDesc = (props) => {
     isFirst,
     isLast,
   } = props;
-  // pl-[1px]
+
   return (
     <div
       className={`
         TIMELINE-DESCRIPTION
+        relative before:absolute before:inset-0
         flex flex-col justify-center
         w-full h-screen max-h-[calc(100vh*(66.25/100))]
-        border-b-black
-        -z-20
+        px-[2.188rem]
         ${(isFirst || isLast)
-        ? `${isLast ? 'bg-gradient-to-t' : 'bg-gradient-to-b'} from-slate-50 to-stone-900`
-        : 'bg-black'}        
+        ? `${isFirst
+          ? 'before:bg-timeline_gradient_utb before:pb-px'
+          : 'before:bg-timeline_gradient_btu '}`
+        : 'before:bg-black before:pb-px'
+        }
         ${isOdd
-          ? `pl-[0.5px] ${!isLast && 'border-b rounded-bl-[12.5rem]'}`
-          : `pr-[1px] ${!isLast && 'border-b rounded-br-[12.5rem]'}`
-        }`}
+          ? `before:pl-px ${!isLast && 'before:rounded-bl-[12.5rem]'}`
+          : `before:pr-px ${!isLast && 'before:rounded-br-[12.5rem]'}`
+        }
+        `}
     >
-      <div className={`
-      wrapper
-      flex flex-col justify-center
-      w-full h-screen max-h-[calc(100vh*(66.25/100))]
-      px-[2.188rem] bg-white
-      ${isOdd
-        ? `${!isLast && 'rounded-bl-[12.5rem]'}`
-        : `${!isLast && 'rounded-br-[12.5rem]'}`
-        }`}
+      <div className="
+        DESCRIPTION-TEXT-CONTAINER
+        flex flex-col"
       >
-        <div className="
-          DESCRIPTION-TEXT-CONTAINER
-          flex flex-col"
-        >
-          <span className="text-base">year</span>
-          <span className="text-[4rem] italic">{title}</span>
-          <span className="text-base font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
-        </div>
+        <span className="text-base">year</span>
+        <span className="text-[4rem] italic">{title}</span>
+        <span className="text-base font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
       </div>
     </div>
   )
