60f7e2f53eebc69713c027b7a61ead29207e24d5
anindyoo <anindyo0216@gmail.com>
2024-05-21 15:51:32 +0700

    feat: create separate element for screen frame.

---
+++ src/components/ScreenFrame/ScreenFrame.jsx
@@ -0,0 +1,16 @@
+const ScreenFrame = () => (
+  <div className="
+      SCREEN-FRAME-CONTAINER
+      fixed z-10
+      w-full h-screen
+      p-3 pointer-events-none"
+  >
+    <div className="
+        SCREEN-FRAME
+        h-full
+        border border-black box"
+    />
+  </div>
+);
+
+export default ScreenFrame;
