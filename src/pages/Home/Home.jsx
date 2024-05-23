832f4bbe902c4841771c9d57a80c9a4f805729a7
anindyoo <anindyo0216@gmail.com>
2024-05-23 20:14:10 +0700

    feat: add top projects data.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,31 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Banner from '../../components/Banner/Banner';
 import Projects from '../../components/Projects/Projects';
 import Experiences from '../../components/Experiences/Experiences';
 import Contact from '../../components/Contact/Contact';
+import projectsData from '../../data/project.json'
 
 const Home = () => {
+  const [topProjects, setTopProjects] = useState([]);
+
+  const compareDate = (a, b) => {
+    const dateA = new Date(a);
+    const dateB = new Date(b);
+    return dateB - dateA;
+  };
+
+  const filterProjects = projectsData.filter((project) => project.topProject);
+  const sortProjects = filterProjects.sort((a, b) => compareDate(a.startDate, b.startDate));
+
   useEffect(() => {
     document.title = 'home - muhammad anindyo poetra mufatyta';
+    setTopProjects(sortProjects);
   }, []);
 
   return (
     <>
       <Banner />
-      <Projects />
+      <Projects projects={topProjects} />
       <Experiences />
       <Contact />
     </>
