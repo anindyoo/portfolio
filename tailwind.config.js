c0a2dd4d046a9aa2011bed54a1d65f0db97f1494
anindyoo <anindyo0216@gmail.com>
2024-05-26 16:31:12 +0700

    feat: add right to left gradient property.

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
       backgroundImage: {
         'timeline_gradient_utb': 'linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(255,255,255,0) 100%)',
         'timeline_gradient_btu': 'linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(255,255,255,0) 100%)',
+        'timeline_gradient_rtl': 'linear-gradient(270deg, rgba(0,0,0,1) 0%, rgba(255,255,255,0) 100%)',
       }     
     },
   },
