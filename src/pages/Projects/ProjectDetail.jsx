cfefdba901c7984adfd01e77593375f17bea8f8c
anindyoo <anindyo0216@gmail.com>
2024-05-26 12:55:27 +0700

    feat: create project detail page.

---
+++ src/pages/Projects/ProjectDetail.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import projectsData from '../../data/project.json';
+import NotFound from '../NotFound/NotFound';
+import ProjectBanner from '../../components/ProjectsComp/ProjectBanner';
+import ProjectDescription from '../../components/ProjectsComp/ProjectDescription';
+import ProjectTechnologies from '../../components/ProjectsComp/ProjectTechnologies';
+import ProjectGallery from '../../components/ProjectsComp/ProjectGallery';
+
+const ProjectDetail = () => {
+  const [project, setProject] = useState({});
+  const { projectId } = useParams();
+
+  useEffect(() => {
+    const projectById = projectsData.find((proj) => proj.id === projectId);
+    setProject(projectById);
+  }, []);
+
+  return (
+    project === undefined
+      ? <NotFound />
+      : (
+        <>
+          <ProjectBanner />
+          <ProjectDescription />
+          <ProjectTechnologies />
+          <ProjectGallery />
+        </>
+      )
+  );
+};
+
+export default ProjectDetail;
