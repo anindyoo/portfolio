c5b29cd75c8082194a4293831f374b7e7669d2ca
anindyoo <anindyo0216@gmail.com>
2024-05-26 12:49:07 +0700

    feat: move compareDate & sortedProjects into useEffect.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,32 +1,32 @@
 import { useEffect, useState } from 'react';
 import Banner from '../../components/Banner/Banner';
-import ProjectsTimeline from '../../components/ProjectsTimeline/ProjectsTimeline';
-import Experiences from '../../components/Experiences/Experiences';
+import ProjectsTimeline from '../../components/ProjectsComp/ProjectsTimeline';
+import ExperiencesMarquee from '../../components/ExperiencesComp/ExperiencesMarquee';
 import Contact from '../../components/Contact/Contact';
 import projectsData from '../../data/project.json'
 
 const Home = () => {
   const [topProjects, setTopProjects] = useState([]);
 
-  const compareDate = (a, b) => {
-    const dateA = new Date(a);
-    const dateB = new Date(b);
-    return dateB - dateA;
-  };
-
-  const filterProjects = projectsData.filter((project) => project.topProject);
-  const sortProjects = filterProjects.sort((a, b) => compareDate(a.startDate, b.startDate));
-
   useEffect(() => {
     document.title = 'home - muhammad anindyo poetra mufatyta';
-    setTopProjects(sortProjects);
+
+    const compareDate = (a, b) => {
+      const dateA = new Date(a);
+      const dateB = new Date(b);
+      return dateB - dateA;
+    };
+
+    const filterProjects = projectsData.filter((project) => project.topProject);
+    const sortedProjects = filterProjects.sort((a, b) => compareDate(a.startDate, b.startDate));
+    setTopProjects(sortedProjects);
   }, []);
 
   return (
     <>
       <Banner />
       <ProjectsTimeline projects={topProjects} isHome />
-      <Experiences />
+      <ExperiencesMarquee />
       <Contact />
     </>
   );
