77ef4fae47d2a3d3ebb1235a6deac53193643563
anindyoo <anindyo0216@gmail.com>
2024-05-26 12:48:10 +0700

    feat: move compareDate & sortedProjects into useEffect.

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,20 +1,20 @@
 import { useEffect, useState } from 'react';
 import projectsData from '../../data/project.json'
-import ProjectsTimeline from '../../components/ProjectsTimeline/ProjectsTimeline';
+import ProjectsTimeline from '../../components/ProjectsComp/ProjectsTimeline';
 
 const Projects = () => {
   const [projects, setProjects] = useState([]);
 
-  const compareDate = (a, b) => {
-    const dateA = new Date(a);
-    const dateB = new Date(b);
-    return dateB - dateA;
-  };
-
-  const sortedProjects = projectsData.sort((a, b) => compareDate(a.startDate, b.startDate));
-
   useEffect(() => {
     document.title = 'projects - muhammad anindyo poetra mufatyta';
+
+    const compareDate = (a, b) => {
+      const dateA = new Date(a);
+      const dateB = new Date(b);
+      return dateB - dateA;
+    };
+
+    const sortedProjects = projectsData.sort((a, b) => compareDate(a.startDate, b.startDate));
     setProjects(sortedProjects)
   }, []);
 
