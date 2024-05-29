e67d7b56924d9d6ee4ac82a156540cda4484487f
anindyoo <anindyo0216@gmail.com>
2024-05-29 11:34:50 +0700

    feat: add link to project detail page.

--- src/components/Timeline/TimelineDesc.jsx
+++ src/components/Timeline/TimelineDesc.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const TimelineDesc = (props) => {
   const {
     project,
@@ -14,32 +16,35 @@ const TimelineDesc = (props) => {
   return (
     <div
       className={`
-        TIMELINE-DESCRIPTION
-        relative before:absolute before:inset-0
-        flex flex-col justify-center
-        w-full h-screen max-h-[calc(100vh*(66.25/100))]
-        px-[2.188rem]
-        before:pointer-events-none
-        ${(isFirst || isLast)
+      TIMELINE-DESCRIPTION
+      relative before:absolute before:inset-0
+      flex flex-col justify-center
+      w-full h-screen max-h-[calc(100vh*(66.25/100))]
+      px-[2.188rem]
+      before:pointer-events-none
+      ${(isFirst || isLast)
         ? `${isFirst
           ? 'before:bg-timeline_gradient_utb before:pb-px'
           : 'before:bg-timeline_gradient_btu '}`
         : 'before:bg-black before:pb-px'
-        }
-        ${isOdd
-          ? `before:pl-px ${!isLast && 'before:rounded-bl-[12.5rem]'}`
-          : `before:pr-px ${!isLast && 'before:rounded-br-[12.5rem]'}`
-        }
-        `}
+      }
+      ${isOdd
+        ? `before:pl-px ${!isLast && 'before:rounded-bl-[12.5rem]'}`
+        : `before:pr-px ${!isLast && 'before:rounded-br-[12.5rem]'}`
+      }
+      `}
     >
-      <div className="
+      <Link
+        to={project.id}
+        className="
         DESCRIPTION-TEXT-CONTAINER
-        flex flex-col"
+        flex flex-col
+        w-fit"
       >
         <span className="text-base font-medium">{finalDate}</span>
         <span className="text-[4rem] italic">{project.title}</span>
         <span className="text-base font-light">{project.subtitle}</span>
-      </div>
+      </Link>
     </div>
   )
 }
--- src/components/Timeline/TimelineImg.jsx
+++ src/components/Timeline/TimelineImg.jsx
@@ -1,5 +1,7 @@
-const TimelineImg = () => {
-  console.log()
+import { Link } from 'react-router-dom';
+
+const TimelineImg = (props) => {
+  const { project } = props;
 
   return (
     <div className={`
@@ -7,14 +9,16 @@ const TimelineImg = () => {
       w-full h-screen max-h-[calc(100vh*(66.25/100))]
       flex items-center justify-center`}
     >
-      <img
-        src="https://i.scdn.co/image/ab67616d0000b2731572698fff8a1db257a53599"
-        alt="alt"
-        className="
-        TIMELINE-IMAGE
-        object-cover
-        w-[calc(100vw*(40.28/100))] h-[calc(100vh*(56.25/100))]"
-      />
+      <Link to={project.id}>
+        <img
+          src="https://i.scdn.co/image/ab67616d0000b2731572698fff8a1db257a53599"
+          alt="alt"
+          className="
+          TIMELINE-IMAGE
+          object-cover
+          w-[calc(100vw*(40.28/100))] h-[calc(100vh*(56.25/100))]"
+        />
+      </Link>
     </div>
   );
 };
