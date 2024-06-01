0dc5471b4702ac9d372dde58e1de58a423c02564
anindyoo <anindyo0216@gmail.com>
2024-06-01 14:01:21 +0700

    feat: add card content using dummy data.

--- src/components/ExperiencesComp/ExperiencesCard.jsx
+++ src/components/ExperiencesComp/ExperiencesCard.jsx
@@ -2,8 +2,24 @@ const ExperiencesCard = () => {
   console.log();
 
   return (
-    <div>
-      test.
+    <div className="
+    EXPERIENCE-CARD
+    border
+    flex flex-col justify-between
+    w-[16.25rem] h-[7.5rem]
+    px-2.5 py-3
+    -skew-y-12
+    hover:cursor-pointer
+    mb-8"
+    >
+      <div className="
+      TITLE-SUBTITLE
+      flex flex-col"
+      >
+        <span className="text-xl font-bold">Card title</span>
+        <span className="text-base font-medium">Card subtitle</span>
+      </div>
+      <div className="DATE text-sm">August 2022-December 2024</div>
     </div>
   );
 };
