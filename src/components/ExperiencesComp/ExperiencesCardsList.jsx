583f58b0457d5da9757fc5d8a3679e23b97f351f
anindyoo <anindyo0216@gmail.com>
2024-06-01 14:02:07 +0700

    feat: add cards list  content using dummy data.

--- src/components/ExperiencesComp/ExperiencesCardsList.jsx
+++ src/components/ExperiencesComp/ExperiencesCardsList.jsx
@@ -1,10 +1,28 @@
+import ExperiencesCard from './ExperiencesCard';
+
 const ExperiencesCardsList = () => {
   console.log();
 
   return (
-    <div>
-      test.
-    </div>
+    <aside className="
+    EXPERIENCES-CARDS-LIST-SECTION
+    w-[16.25rem]"
+    >
+      <div className="
+      EXPERIENCES-CARDS-LIST-SCROLL
+      absolute top-0
+      max-h-screen
+      overflow-y-scroll"
+      >
+        <div className="
+        EXPERIENCES-CARDS-LIST-CONTAINER
+        py-[5.625rem]"
+        >
+          {[...'1234567890'].map((x) => <ExperiencesCard key={x} />)}
+        </div>
+      </div>
+
+    </aside>
   );
 };
 
