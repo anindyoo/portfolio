905e78473c28afc02bfc5cbe7e20294fceb03e9d
anindyoo <anindyo0216@gmail.com>
2024-06-01 18:07:48 +0700

    feat: add article content using dummy data.

--- src/components/ExperiencesComp/ExperiencesArticle.jsx
+++ src/components/ExperiencesComp/ExperiencesArticle.jsx
@@ -1,10 +1,109 @@
+/* eslint-disable max-len */
 const ExperiencesArticle = () => {
   console.log();
 
   return (
-    <div>
-      test.
-    </div>
+    <article className="
+    EXPERIENCE-ARTICLE
+    flex flex-col gap-[3.125rem]
+    h-fit
+    my-[1.875rem]
+    px-2.5 pb-28
+    overflow-hidden"
+    >
+      <section className="
+      ARTICLE-HEADER
+      flex flex-col gap-2
+      mb-[1.875rem]"
+      >
+        <h1 className="text-[2.5rem] font-semibold tracking-[-0.04em]">Front End Engineer Specialist</h1>
+        <div className="
+        HEADER-INFOS
+        flex flex-col gap-1.5
+        text-xl font-medium tracking-tight"
+        >
+          <h2 className="flex flex-row gap-2.5 items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z" />
+            </svg>
+            Company Name
+          </h2>
+          <h2 className="flex flex-row gap-2.5 items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+              <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+            </svg>
+            Jakarta, Indonesia
+          </h2>
+          <h2 className="flex flex-row gap-2.5 items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+            </svg>
+            August 2022—November 2022
+          </h2>
+        </div>
+      </section>
+      <section className="
+      ARTICLE-PREFACE
+      flex flex-col gap-3"
+      >
+        <h1 className="text-2xl font-medium tracking-[-0.08em] italic">preface to the experience</h1>
+        <p className="leading-relaxed">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vestibulum mi eros, ac imperdiet orci placerat ut. Aenean sollicitudin velit purus, eu consectetur lorem porta ut. Ut et faucibus magna. Cras eu laoreet felis. Curabitur mi lacus, convallis et risus ac, fringilla ultrices ligula. Morbi tempus tristique ligula, lobortis vehicula diam convallis id. Aenean suscipit justo at augue ornare, a dignissim sem facilisis.
+          <br />
+          <br />
+          Praesent congue dapibus euismod. Donec eget vulputate est. Fusce sollicitudin magna sit amet tellus dictum molestie. Pellentesque quis elit lacinia, hendrerit purus et, porta orci. Suspendisse quis egestas purus. Integer condimentum ligula felis, quis consequat ex lacinia ullamcorper. Sed sit amet felis sit amet nibh rutrum tristique. Fusce nec blandit est. Nunc fringilla magna nunc, sed venenatis nunc fringilla sed. Donec et sapien at ipsum viverra interdum sit amet in enim.
+          <br />
+          <br />
+          Ut fringilla, libero placerat tempus vehicula, odio felis cursus mauris, nec mollis arcu felis sit amet neque. Cras ultrices pellentesque metus quis faucibus. Praesent tincidunt pulvinar metus, vitae ultricies erat feugiat ac. Curabitur vitae dignissim velit. Duis varius nulla ut arcu suscipit dictum viverra id magna. In et magna odio. Aliquam vestibulum lorem vitae lectus congue, quis viverra mauris tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+        </p>
+      </section>
+      <section className="
+      ARTICLE-MAIN
+      flex flex-col gap-3"
+      >
+        <h1 className="text-2xl font-medium tracking-[-0.08em] italic">the experience</h1>
+        <p className="leading-relaxed">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vestibulum mi eros, ac imperdiet orci placerat ut. Aenean sollicitudin velit purus, eu consectetur lorem porta ut. Ut et faucibus magna. Cras eu laoreet felis. Curabitur mi lacus, convallis et risus ac, fringilla ultrices ligula. Morbi tempus tristique ligula, lobortis vehicula diam convallis id. Aenean suscipit justo at augue ornare, a dignissim sem facilisis.
+          <br />
+          <br />
+          Praesent congue dapibus euismod. Donec eget vulputate est. Fusce sollicitudin magna sit amet tellus dictum molestie. Pellentesque quis elit lacinia, hendrerit purus et, porta orci. Suspendisse quis egestas purus. Integer condimentum ligula felis, quis consequat ex lacinia ullamcorper. Sed sit amet felis sit amet nibh rutrum tristique. Fusce nec blandit est. Nunc fringilla magna nunc, sed venenatis nunc fringilla sed. Donec et sapien at ipsum viverra interdum sit amet in enim.
+        </p>
+      </section>
+      <section className="
+      ARTICLE-PROJECTS
+      flex flex-col gap-3"
+      >
+        <h1 className="text-2xl font-medium tracking-[-0.08em] italic">associated projects</h1>
+        <p className="leading-relaxed">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vestibulum mi eros, ac imperdiet orci placerat ut. Aenean sollicitudin velit purus, eu consectetur lorem porta ut. Ut et faucibus magna. Cras eu laoreet felis. Curabitur mi lacus, convallis et risus ac, fringilla ultrices ligula. Morbi tempus tristique ligula, lobortis vehicula diam convallis id. Aenean suscipit justo at augue ornare, a dignissim sem facilisis.
+        </p>
+      </section>
+      <section className="
+      ARTICLE-CERTIFICATES
+      flex flex-col gap-3"
+      >
+        <h1 className="text-2xl font-medium tracking-[-0.08em] italic">certificates & credentials</h1>
+        <p className="leading-relaxed">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vestibulum mi eros, ac imperdiet orci placerat ut. Aenean sollicitudin velit purus, eu consectetur lorem porta ut. Ut et faucibus magna. Cras eu laoreet felis. Curabitur mi lacus, convallis et risus ac, fringilla ultrices ligula. Morbi tempus tristique ligula, lobortis vehicula diam convallis id. Aenean suscipit justo at augue ornare, a dignissim sem facilisis.
+        </p>
+      </section>
+      <section className="
+      ARTICLE-LINKS
+      flex flex-col gap-3"
+      >
+        <h1 className="text-2xl font-medium tracking-[-0.08em] italic">necessary links</h1>
+        <p className="leading-relaxed">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vestibulum mi eros, ac imperdiet orci placerat ut. Aenean sollicitudin velit purus, eu consectetur lorem porta ut. Ut et faucibus magna. Cras eu laoreet felis. Curabitur mi lacus, convallis et risus ac, fringilla ultrices ligula. Morbi tempus tristique ligula, lobortis vehicula diam convallis id. Aenean suscipit justo at augue ornare, a dignissim sem facilisis.
+        </p>
+      </section>
+      <section className="
+      ARTICLE-GALLERY
+      flex flex-col gap-3"
+      >
+        <h1 className="text-2xl font-medium tracking-[-0.08em] italic">gallery</h1>
+      </section>
+    </article>
   );
 };
 
