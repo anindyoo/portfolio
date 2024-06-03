f8312f7f516a6149d81006e06763b6592180c66f
anindyoo <anindyo0216@gmail.com>
2024-06-03 14:11:18 +0700

    feat: add experiences data from json & implement experience article rendering based on selected card.

--- src/pages/Experiences/Experiences.jsx
+++ src/pages/Experiences/Experiences.jsx
@@ -1,11 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import ExperiencesCardsList from '../../components/ExperiencesComp/ExperiencesCardsList';
 import ExperiencesArticle from '../../components/ExperiencesComp/ExperiencesArticle';
+import experiencesData from '../../data/experience.json';
+import helpers from '../../helpers/helpers';
 
 const Experiences = () => {
+  const [experiences, setExperiences] = useState([]);
+  const [activeCard, setActiveCard] = useState(null);
+
+  const activeCardClickHandler = (exp) => {
+    setActiveCard(exp);
+  }
+
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [])
+
+    const sortedExperiences = helpers.sortByDate(experiencesData);
+    setExperiences(sortedExperiences);
+    setActiveCard(sortedExperiences[0]);
+  }, []);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [activeCard]);
 
   return (
     <div className="
@@ -13,8 +30,12 @@ const Experiences = () => {
     flex flex-row
     max-h-[calc(100vh-20px)]"
     >
-      <ExperiencesCardsList />
-      <ExperiencesArticle />
+      <ExperiencesCardsList
+        experiences={experiences}
+        activeCardId={activeCard?.id}
+        activeCardClickHandler={activeCardClickHandler}
+      />
+      <ExperiencesArticle activeCard={activeCard} />
     </div>
   );
 };
