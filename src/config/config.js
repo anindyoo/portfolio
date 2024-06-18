edc952d41f1b1276d0a3135a15afe7795f362ad3
anindyoo <anindyo0216@gmail.com>
2024-06-18 15:28:26 +0700

    feat: add screen breakpoints config.

---
+++ src/config/config.js
@@ -0,0 +1,13 @@
+const screenBreakpoints = {
+  mobileSmall: 320,
+  mobileMedium: 375,
+  mobileLarge: 425,
+  tablet: 768,
+  laptop: 1024,
+  laptopLarge: 1440,
+  uhd: 2560,
+};
+
+export default {
+  screenBreakpoints,
+};
