188ae23022a661e6376920270ef476a8cbe9668e
anindyoo <anindyo0216@gmail.com>
2024-06-19 17:18:30 +0700

    feat: add navbar links conditional rendering for mobile screens.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import MenuNavLink from '../MenuNavLink/MenuNavLink';
+import useScreenSize from '../../hooks/useScreenSize';
+import config from '../../config/config';
 
 const menuNavlinksClassName = (menu) => `MENU-NAVLINK-${menu}`;
 
@@ -10,33 +12,41 @@ const menuNavlinks = [
   { name: 'contact', link: '/contact', className: menuNavlinksClassName('CONTACT') },
 ];
 
-const Navbar = () => (
-  <nav className="
+const Navbar = () => {
+  const screenSize = useScreenSize();
+  const screenWidth = screenSize.width;
+
+  return (
+    <nav className="
     NAVBAR
     fixed z-50
     flex flex-row justify-between
-    w-full
+    w-full max-w-[125rem]
     px-7 py-5
     text-2xl tracking-[-0.08em]"
-  >
-    <span className="NAVBAR-LOGO HEADLINE-TEXT decoration-lightSweetener italic hover:underline">
-      <NavLink to="/" className="pr-4">
-        anindyo
-      </NavLink>
-    </span>
-    <span className="
-      NAVBAR-LINKS
-      flex flex-row gap-7"
     >
-      {menuNavlinks.map((navlink) => (
-        <MenuNavLink
-          className={navlink.className}
-          key={`menu-navlink-${navlink.link}`}
-          name={navlink.name}
-          link={navlink.link}
-        />
-      ))}
-    </span>
-  </nav>
-);
+      <span className="NAVBAR-LOGO HEADLINE-TEXT decoration-lightSweetener italic hover:underline">
+        <NavLink to="/" className="pr-4">
+          anindyo
+        </NavLink>
+      </span>
+      {screenWidth > config.screenBreakpoints.tablet && (
+        <span className="
+        NAVBAR-LINKS
+        flex flex-row gap-7"
+        >
+          {menuNavlinks.map((navlink) => (
+            <MenuNavLink
+              className={navlink.className}
+              key={`menu-navlink-${navlink.link}`}
+              name={navlink.name}
+              link={navlink.link}
+            />
+          ))}
+        </span>
+      )}
+    </nav>
+  );
+};
+
 export default Navbar;
