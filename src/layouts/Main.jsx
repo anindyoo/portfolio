89a24325919507beefd114215feef8434675daed
anindyoo <anindyo0216@gmail.com>
2024-06-27 22:27:48 +0700

    feat: add navbar conditional rendering based on screen width.

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,6 +1,8 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar/Navbar';
 import Footer from '../components/Footer/Footer';
+import useScreenSize from '../hooks/useScreenSize';
+import config from '../config/config';
 
 const Main = (props) => {
   const {
@@ -8,13 +10,17 @@ const Main = (props) => {
     noFooter,
   } = props;
 
+  const screenSize = useScreenSize();
+  const screenWidth = screenSize.width;
+  const laptopBreakpoint = config.screenBreakpoints.laptop;
+
   return (
     <>
-      {!noNavbar && (
-        <header>
-          <Navbar />
-        </header>
-      )}
+      <header>
+        {noNavbar && (screenWidth < laptopBreakpoint) ? (
+          <Navbar noLogo />
+        ) : <Navbar />}
+      </header>
       <main className="
       MAIN-CONTAINER
       px-5 lg:px-7 py-5"
