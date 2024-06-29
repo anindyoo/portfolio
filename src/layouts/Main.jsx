e9e4f1b14c0d78f95c9608f248c9d8681263e09d
anindyoo <anindyo0216@gmail.com>
2024-06-29 12:38:17 +0700

    refactor: remove unused screen breakpoint conditional rendering.

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,25 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar/Navbar';
 import Footer from '../components/Footer/Footer';
-import useScreenSize from '../hooks/useScreenSize';
-import config from '../config/config';
 
 const Main = (props) => {
   const {
-    noNavbar,
     noFooter,
   } = props;
-
-  const screenSize = useScreenSize();
-  const screenWidth = screenSize.width;
-  const laptopBreakpoint = config.screenBreakpoints.laptop;
 
   return (
     <>
       <header>
-        {noNavbar && (screenWidth < laptopBreakpoint) ? (
-          <Navbar noLogo />
-        ) : <Navbar />}
+        <Navbar {...props} />
       </header>
       <main className="
       MAIN-CONTAINER
