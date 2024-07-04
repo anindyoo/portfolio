0c8c89bb8f1ce8a0d6c82028dbab51907ea2d72e
anindyoo <anindyo0216@gmail.com>
2024-07-04 13:19:15 +0700

    feat: create contact page.

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,15 @@
+import ContactBanner from '../../components/ContactBanner/ContactBanner';
+
+const Contact = () => {
+  console.log()
+  return (
+    <div className="
+    CONTACT-PAGE
+    h-screen"
+    >
+      <ContactBanner />
+    </div>
+  )
+}
+
+export default Contact;
