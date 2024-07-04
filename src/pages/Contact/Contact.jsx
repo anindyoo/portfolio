2e1232bc55a923d12e324d4fa93b53aa98cb6aa0
anindyoo <anindyo0216@gmail.com>
2024-07-04 13:19:41 +0700

    refactor: remove uused console log.

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,15 +1,12 @@
 import ContactBanner from '../../components/ContactBanner/ContactBanner';
 
-const Contact = () => {
-  console.log()
-  return (
-    <div className="
+const Contact = () => (
+  <div className="
     CONTACT-PAGE
     h-screen"
-    >
-      <ContactBanner />
-    </div>
-  )
-}
+  >
+    <ContactBanner />
+  </div>
+)
 
 export default Contact;
