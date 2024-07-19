3b8f653909764eab8ca7e1355db2a5b894fcf16e
anindyoo <anindyo0216@gmail.com>
2024-07-19 16:28:27 +0700

    feat: add scroll to top useEffect.

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,12 +1,20 @@
+import { useEffect } from 'react';
 import ContactBanner from '../../components/ContactBanner/ContactBanner';
 
-const Contact = () => (
-  <div className="
-  CONTACT-PAGE
-  min-h-screen"
-  >
-    <ContactBanner />
-  </div>
-);
+const Contact = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    document.title = 'about — muhammad anindyo poetra mufatyta';
+  }, []);
+
+  return (
+    <div className="
+    CONTACT-PAGE
+    min-h-screen"
+    >
+      <ContactBanner />
+    </div>
+  );
+};
 
 export default Contact;
